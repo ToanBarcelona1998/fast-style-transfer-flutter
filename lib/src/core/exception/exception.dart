48365be48d700b405a6dc6e67a772c295830b659
ToanBarcelona1998 <77067868+ToanBarcelona1998@users.noreply.github.com>
2025-07-12 18:05:20 +0700

    feat: add style transfer exception

---
+++ lib/src/core/exception/exception.dart
@@ -0,0 +1,6 @@
+final class FastStyleTransferException {
+  final String msg;
+  final int code;
+
+  const FastStyleTransferException({required this.msg, required this.code});
+}
---
+++ lib/src/core/exception/exception_code.dart
@@ -0,0 +1,7 @@
+final class FastStyleTransferExceptionCode {
+  const FastStyleTransferExceptionCode._();
+
+  static const int decodeImageError  = 1;
+  static const int nonSupportLoaderError  = 2;
+  static const int nonInitError  = 2;
+}
